59abf6012e634dc5691f8495eea185c05a739e66
SergeyMysin <sergey.mysin@gmail.com>
2019-12-13 14:53:25 -0500

    Plasma shooting test scene

---
+++ AlienZZ/Assets/Scenes/Sergey_Test/PlasmaBall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaBall : MonoBehaviour
+{
+    GameObject green;
+    GameObject red;
+
+    GameObject greenBullet;
+    GameObject redBullet;
+
+    private void Awake()
+    {
+        green = GetComponentInChildren<ID_Green>().gameObject;
+        red = GetComponentInChildren<ID_Red>().gameObject;
+        greenBullet = Resources.Load(("Prefabs/PlasmaBulletGreen"), typeof(GameObject)) as GameObject;
+        redBullet = Resources.Load(("Prefabs/PlasmaBulletRed"), typeof(GameObject)) as GameObject;
+    }
+
+    private void Start()
+    {
+        red.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.G) && !green.activeSelf)
+        {
+            red.SetActive(false);
+            green.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.R) && !red.activeSelf)
+        {
+            green.SetActive(false);
+            red.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (green.activeSelf)
+            {
+                Instantiate(greenBullet, new Vector3(0, 0, 0.5f), Quaternion.Euler(90, 0, -90));
+            }
+            if (red.activeSelf)
+            {
+                Instantiate(redBullet, new Vector3(0, 0, 0.5f), Quaternion.Euler(90, 0, -90));
+            }
+        }
+    }
+}
---
+++ AlienZZ/Assets/Scenes/Sergey_Test/PlasmaBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaBullet : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0, 0, 0.1f);
+        StartCoroutine("Countdown");
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+}
