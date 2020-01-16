a0ee89d09a1e901dc95369f9fdc50bb0c5f1aa97
rohit-lad <36147572+rohit-lad@users.noreply.github.com>
2020-01-16 16:22:16 -0500

    Ammo Text Script (Sergey)

---
+++ AlienZZ/Assets/Scenes/Sergey_Test/Scripts/AmmoGunText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoGunText : MonoBehaviour
+{
+    GunTestVR gtScriptVR;
+    Text ammoGun;
+    private void Awake()
+    {
+        gtScriptVR = FindObjectOfType<GunTestVR>();
+        ammoGun = GetComponent<Text>();
+    }
+    private void FixedUpdate()
+    {
+        int ag = gtScriptVR.currAmmo;
+        ammoGun = ag.
+    }
+}
