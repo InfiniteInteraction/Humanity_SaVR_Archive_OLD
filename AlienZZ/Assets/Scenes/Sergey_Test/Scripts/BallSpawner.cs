0ae371c06c565dd3ffef62556d97a02012bc07ed
SergeyMysin <sergey.mysin@gmail.com>
2020-01-31 16:15:49 -0500

    New testing setup

---
+++ AlienZZ/Assets/Scenes/Sergey_Test/Scripts/BallSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    GameObject prefab;
+    Transform spawnPoint;
+    bool spawnNow = true;
+
+    private void Awake()
+    {
+        prefab = Resources.Load(("Prefabs/Alien"), typeof(GameObject)) as GameObject;
+        spawnPoint = GetComponentInChildren<ID_BallSpawn>().transform;
+    }
+
+    private void Update()
+    {
+        if (spawnNow)
+        {
+            StartCoroutine("SpawnBalls");
+        }
+    }
+
+    IEnumerator SpawnBalls()
+    {
+        spawnNow = false;
+        Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+        yield return new WaitForSeconds(5f);
+        spawnNow = true;
+    }
+}
