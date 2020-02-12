ad8c2b0840e652a882255788067f81e61c5018d2
SergeyMysin <sergey.mysin@gmail.com>
2020-02-12 15:49:47 -0500

    Implementing the menu system

---
+++ AlienZZ/Assets/Scripts/ButtonPush.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPush : MonoBehaviour
+{
+    public StartMenuFunctionality sMU;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        if(gameObject.tag == "RedBullet")
+        {
+            sMU.NewGame();
+        }
+    }
+}
