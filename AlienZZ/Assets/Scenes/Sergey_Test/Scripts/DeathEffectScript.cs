ae8183190ee94277979ab8b9962e45647f4636b2
SergeyMysin <sergey.mysin@gmail.com>
2020-02-20 23:27:40 -0500

    Enemy death particle effects

--- AlienZZ/Assets/Scenes/Sergey_Test/Scripts/DeathEffectScript.cs
+++ AlienZZ/Assets/Scenes/Sergey_Test/Scripts/DeathEffectScript.cs
@@ -4,25 +4,9 @@ using UnityEngine;
 
 public class DeathEffectScript : MonoBehaviour
 {
-    public float alphaMinus = 0;
-    Color psColor;
-
     private void Awake()
     {
-        psColor.a = 0.9f;
         StartCoroutine("Destroy");
-    }
-    private void FixedUpdate()
-    {
-        alphaMinus += Time.fixedDeltaTime * 50;
-        psColor.a -= alphaMinus;
-        //DeathEffect();
-    }
-
-    void DeathEffect()
-    {
-        Material mat = GetComponent<Material>();
-        mat.color = psColor;
     }
 
     IEnumerator Destroy()
