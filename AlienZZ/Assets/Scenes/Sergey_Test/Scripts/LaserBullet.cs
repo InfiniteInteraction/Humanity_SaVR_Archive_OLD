755ef7c11e321150c5e28210a5eb3d2b7e8e1e1d
T-Rich23 <58788425+T-Rich23@users.noreply.github.com>
2020-03-04 16:09:59 -0500

    Updates by Sergey
    - Bullet prefabs fixed
    - Navmesh and static objects in the scene updated

--- AlienZZ/Assets/Scenes/Sergey_Test/Scripts/LaserBullet.cs
+++ AlienZZ/Assets/Scenes/Sergey_Test/Scripts/LaserBullet.cs
@@ -81,7 +81,7 @@ public class LaserBullet : MonoBehaviour
     {
         if (rb)
         {
-            rb.AddForce(transform.right * -200);
+            rb.AddForce(transform.right * -250);
             StartCoroutine("Countdown");
         }
     }
